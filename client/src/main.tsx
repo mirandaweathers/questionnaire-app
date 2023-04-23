4756d519f8b48e635c737aad2518eec1e6df5b33
mirandaweathers <mirandaweathers@gmail.com>
2023-04-23 13:49:40 -0400

    begin using router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ClientDetailForm from './components/ClientDetailForm';
+import { DesignerDashboard } from './components/DesignerDashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/client-details",
+    element: <ClientDetailForm />
+  },
+  {
+    path: "/dashboard",
+    element: <DesignerDashboard />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 )
